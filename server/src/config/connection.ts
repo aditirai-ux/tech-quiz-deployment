f6d86c5a22158f63dcb9463c1acf5928f0376c2c
Aditi Rai <aditi45@gmail.com>
2024-11-08 20:39:30 +0530

    reverting changes to cleanDB and connection.ts

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,18 +3,6 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || '';
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
 
-const db = async (): Promise<typeof mongoose.connection> => {
-    try {
-        await mongoose.connect(MONGODB_URI);
-        console.log('Connected to the database');
-        return mongoose.connection;
-    } catch (error) {
-        console.error('Could not connect to the database', error);
-        throw new Error('Database connection failed');
-    }
-};
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
-
-export default db;
+export default mongoose.connection;
