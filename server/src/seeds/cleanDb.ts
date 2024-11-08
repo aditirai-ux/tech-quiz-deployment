35262bda559b13ea5966fc38830e00184996962f
Aditi Rai <aditi45@gmail.com>
2024-11-08 08:59:16 +0530

    fixing errors in cleanDB.ts

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,11 +1,17 @@
-import models from '../models/index.js';
+// import Question from '../models/index.js';
 import db from '../config/connection.js';
+import models from '../models/index.js';
 
-export default async (modelName: "Question" | undefined, collectionName: string) => {
+export default async (modelName: keyof typeof models , collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    let modelExists = [];
+    if (models[modelName] && models[modelName].db) {
+      if (models[modelName].db.db) {
+        modelExists = await models[modelName].db.db.listCollections({
+          name: collectionName
+        }).toArray();
+      }
+    }
 
     if (modelExists.length) {
       await db.dropCollection(collectionName);
