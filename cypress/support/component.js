a019a10a0365164a176ec3c076b79876957bf668
Aditi Rai <aditi45@gmail.com>
2024-11-08 21:55:07 +0530

    pending changes

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -21,14 +21,6 @@ import './commands'
 
 import { mount } from 'cypress/react18'
 
-declare global {
-    namespace Cypress {
-      interface Chainable {
-        mount: typeof mount
-      }
-    }
-  }
-
 Cypress.Commands.add('mount', mount)
 
 // Example use:
