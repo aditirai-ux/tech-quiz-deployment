41844eeea0f642f0422ab04c870c9e1460c3b3b1
Aditi Rai <aditi45@gmail.com>
2024-11-08 10:00:40 +0530

    modified connection.ts

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,6 +3,18 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+const MONGODB_URI = process.env.MONGODB_URI || '';
 
-export default mongoose.connection;
+const db = async (): Promise<typeof mongoose.connection> => {
+    try {
+        await mongoose.connect(MONGODB_URI);
+        console.log('Connected to the database');
+        return mongoose.connection;
+    } catch (error) {
+        console.error('Could not connect to the database', error);
+        throw new Error('Database connection failed');
+    }
+};
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+
+export default db;
