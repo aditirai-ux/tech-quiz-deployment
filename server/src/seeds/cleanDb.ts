db858b6fcc5dc8bef25c8a4cc9e8b729bd3d29f7
Aditi Rai <aditi45@gmail.com>
2024-11-08 08:50:55 +0530

    tweaking the code in cleanDB to address type errors

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,7 +1,7 @@
 import models from '../models/index.js';
 import db from '../config/connection.js';
 
-export default async (modelName: "Question", collectionName: string) => {
+export default async (modelName: "Question" | undefined, collectionName: string) => {
   try {
     let modelExists = await models[modelName].db.db.listCollections({
       name: collectionName
